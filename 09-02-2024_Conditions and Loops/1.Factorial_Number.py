24a29f8ead44d646148cb6550c1fd522fd60a427
Nilesh-Nikume <nikumenileh@gmail.com>
2024-02-12 18:36:47 +0530

    12Feb2024_Task

--- 09-02-2024_Conditions and Loops/1.Factorial_Number.py
+++ 09-02-2024_Conditions and Loops/1.Factorial_Number.py
@@ -1,9 +1,9 @@
-n = 6
-fact = 1
-for i in range(1,n+1):
-    fact = fact * i
-print(fact)
-
+# n = 6
+# fact = 1
+# for i in range(1,n+1):
+#     fact = fact * i
+# print(fact)
+#
 def fact(Number):
     Fact_Number = 1
     for i in range(1, Number+1):
@@ -11,4 +11,12 @@ def fact(Number):
     print("Factorial is: ", Fact_Number)
 Number = int(input("Enter the Number: "))
 fact(Number)
+
+# import math
+# Number = int(input("Enter the Number: "))
+# fact_no = math.factorial(Number)
+# print(fact_no)
+# # fibo_No = math.fibonaccie(Number)
+
+
 
